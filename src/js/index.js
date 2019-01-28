c42948bc24071b2d461c6a0e47f6a687b5f1f0ad
Fidalgo <pedrofidalgo7@gmail.com>
2019-01-28 16:54:51 +0000

    First commit Project setup, webpack all setup with babel-loader, using the preset-env Sass being converted do CSS, then into commonJS and beng loaded into the DOM

---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+require('../css/main.scss');
+
+console.log('test');
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: ['babel-polyfill', './src/js/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+    new Dotenv(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', // creates style nodes from JS strings
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader', // compiles Sass to CSS, using Node Sass by default
+        ],
+      },
+    ],
+  },
+};
